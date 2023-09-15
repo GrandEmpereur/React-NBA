e7640356180c9ffdb7ca8279b8cb2b565fcf5d5a
Empereur <bartosikpatrick2@gmail.com>
2023-09-15 08:57:10 +0100

    first commit

---
+++ src/components/App.tsx
@@ -0,0 +1,26 @@
+import Filter from "./FilterComponent";
+import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
+
+function App() {
+	const user = useContext(AuthContext);
+
+	if (user && user.isLogged) {
+		return (
+			<div className="App">
+				<Filter />
+			</div>
+		);
+	}
+
+	return (
+		<div className="App text-center">
+			<h1 className="mb-10">Not logged</h1>
+			<a href="/" className="text-white">
+				<button>Go to login page</button>
+			</a>
+		</div>
+	);
+}
+
+export default App;
